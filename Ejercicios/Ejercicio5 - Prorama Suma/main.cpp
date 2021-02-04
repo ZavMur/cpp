864b210f5e36da8abc3432f8067ec9184023a7f9
Bryan ZavMur <bf.murillo@unah.hn>
2021-02-03 19:10:20 -0600

    subiendo suma

---
+++ Ejercicios/Ejercicio5 - Prorama Suma/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	// Datos de entrada
+	
+	int a = 0;
+	int b = 0;
+	int resultado = 0;
+	
+	cout << "Ingrese el valor de a: ";
+	cin >> a;
+	
+	cout<<endl;
+	
+	cout << "Ingrese el valor de b: ";
+	cin >> b;	
+	
+	//proceso
+	resultado = a + b;
+	
+	//salida
+	cout<< endl;
+	cout << "La suma de a + b es: "<< resultado;
+	
+	return 0;
+}
